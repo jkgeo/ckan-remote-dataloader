479b840422d18d3655f8efec08804683f990b5c4
John <jfkeniston@gmail.com>
2020-08-17 20:33:27 -0400

    add initial program

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+from io import open
+from os import path
+
+import pathlib
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+with open(path.join(HERE, 'requirements.txt'), encoding='utf-8') as f:
+    all_reqs = f.read().split('\n')
+
+
+install_requires = [x.strip() for x in all_reqs if ('git+' not in x) and (
+    not x.startswith('#')) and (not x.startswith('-'))]
+dependency_links = [x.strip().replace('git+', '') for x in all_reqs \
+                    if 'git+' not in x]
