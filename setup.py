3a33e344656a10349261369b35f3ac266cac6107
John <jfkeniston@gmail.com>
2020-08-17 20:42:57 -0400

    add setup.py

--- setup.py
+++ setup.py
@@ -18,3 +18,30 @@ install_requires = [x.strip() for x in all_reqs if ('git+' not in x) and (
     not x.startswith('#')) and (not x.startswith('-'))]
 dependency_links = [x.strip().replace('git+', '') for x in all_reqs \
                     if 'git+' not in x]
+
+setup (
+    name = 'ckan-remote-dataloader',
+    description = 'A simple commandline app for searching and looking up opensource vulnerabilities',
+    version = '0.0.0',
+    packages = find_packages(), 
+    install_requires = install_requires,
+    python_requires='>=3', 
+    entry_points='''
+        [console_scripts]
+        ckanloader=ckanloader.__init__:main
+    ''',
+    author="John Keniston",
+    keyword="ckan, datastore, remote, loader",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    license='MIT',
+    url='https://github.com/jkgeo/ckan-remote-dataloader',
+    # download_url='https://github.com/CITGuru/cver/archive/1.0.0.tar.gz',
+    dependency_links=dependency_links,
+    author_email='jfkeniston@gmail.com',
+    classifiers=[
+        "License :: OSI Approved :: MIT License",]
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ]
+)
