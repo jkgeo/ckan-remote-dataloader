49253288e6b3138f87c2460e4c5f553eebead6dd
John <jfkeniston@gmail.com>
2020-08-17 22:08:34 -0400

    trying to fix requirements

--- setup.py
+++ setup.py
@@ -9,15 +9,21 @@ HERE = pathlib.Path(__file__).parent
 # The text of the README file
 README = (HERE / "README.md").read_text()
 
+# automatically captured required modules for install_requires in requirements.txt
+with open(path.join(HERE, 'requirements.txt'), encoding='utf-8') as f:
+    all_reqs = f.read().split('\n')
+
+install_requires = [x.strip() for x in all_reqs if ('git+' not in x) and (
+    not x.startswith('#')) and (not x.startswith('-'))]
+dependency_links = [x.strip().replace('git+', '') for x in all_reqs \
+                    if 'git+' not in x]
+
 setup (
     name = 'ckan-remote-dataloader',
     description = 'A simple commandline app for uploading tables to a remote CKAN datastore',
-    version = '0.0.3',
+    version = '0.0.6',
     packages = find_packages(), 
-    install_requires = [
-        'click',
-        'messytables'
-    ],
+    install_requires = install_requires,
     python_requires='>=3.6', 
     entry_points='''
         [console_scripts]
@@ -29,10 +35,8 @@ setup (
     long_description_content_type="text/markdown",
     license='MIT',
     url='https://github.com/jkgeo/ckan-remote-dataloader',
-    download_url='https://github.com/jkgeo/ckan-remote-dataloader/archive/v0.0.0.tar.gz',
-    dependency_links=[
-        'https://github.com/jkgeo/ckanapi.git@f30afe2a788844188656f2eb0ef7f0e0e11186a9#egg=ckanapi'
-    ],
+    # download_url='https://github.com/jkgeo/ckan-remote-dataloader/archive/v0.0.0.tar.gz',
+    dependency_links= dependency_links,
     author_email='jfkeniston@gmail.com',
     classifiers=[
         "License :: OSI Approved :: MIT License",
